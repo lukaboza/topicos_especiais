accea2fc48eb91c4163cc88956eee8649ab507ce
HaissamHammoudFawaz <haissam_fawaz_4@hotmail.com>
2022-05-07 00:37:04 +0000

    add html

--- app/models.py
+++ app/models.py
@@ -2,25 +2,29 @@ from django.db import models
 
 # Create your models here.
 
+# mock ---
+class Professor(models.Model):
+    nome = models.CharField(max_length=20)
 
-class Endereco(models.Model):
-    rua = models.CharField(max_length=30)
-    numero = models.CharField(max_length=10)
-    bairro = models.CharField(max_length=20)
-    cidade = models.CharField(max_length=20)
-    complemento = models.CharField(max_length=20)
+class Escolas(models.Model):
+    nome = models.CharField(max_length=20)
 
-    cep = models.CharField(max_length=8)
 
-    def __str__(self):
-        return "oi"
+class Estilos(models.Model):
+    nome = models.CharField(max_length=20)
+
 
 class Alvara(models.Model):
-    id_escola = models.CharField(max_length=30) # aqui sera o id da escola
+    id_escola = models.ForeignKey(Escolas,on_delete=models.DO_NOTHING) # aqui sera o id da escola
     data_de_criacao = models.DateField()
+    valido_ate = models.DateField()
     qrcode = models.ImageField()
-    estilo = models.CharField(max_length=30)
-    endereco = models.OneToOneField(Endereco, on_delete=models.CASCADE)
-
+    id_responsavel = models.ForeignKey(Professor, on_delete=models.DO_NOTHING)
+    
     def __str__(self):
         return "oi"
+
+
+class AlvaraEstilos(models.Model):
+    id_alvara = models.ForeignKey(Alvara, on_delete=models.DO_NOTHING)
+    id_estilos = models.ForeignKey(Estilos, on_delete=models.DO_NOTHING)
