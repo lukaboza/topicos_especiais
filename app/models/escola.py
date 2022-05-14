4634ca9808cafadcf076f22eb92a3524e4ebdf56
HaissamHammoudFawaz <haissam_fawaz_4@hotmail.com>
2022-05-14 00:31:39 +0000

    Ajusta os modelos criados

--- app/models/escola.py
+++ app/models/escola.py
@@ -9,4 +9,3 @@ class Escola(models.Model):
     federacao = models.ForeignKey(Federacao, models.DO_NOTHING)
     razao_social = models.CharField(max_length=45)
     nome_fantasia = models.CharField(max_lenght=45)
-
--- app/models/federacao.py
+++ app/models/federacao.py
@@ -3,15 +3,15 @@ from app.models.federacao import Professor
 
 from app.models.responsavel_atleta import ResponsavelAtleta
 
-class Federacao(models.Model):
+class Escola(models.Model):
     class Meta:
-        db_table = 'federacoes'
+        db_table = 'escola'
 
     nome_fantasia = models.CharField(max_length=255)
     razao_social = models.CharField(max_length=255)
     cnpj = models.CharField(max_length=14, db_index=True)
     email = models.CharField(max_length=255)
-    presidente = models.ForeignKey(Professor)
+    presidente = models.ForeignKey(Professor, on_delete=models.DO_NOTHING)
 
     criado_em = models.DateTimeField(auto_now_add=True)
     atualizado_em = models.DateTimeField(auto_now=True)
